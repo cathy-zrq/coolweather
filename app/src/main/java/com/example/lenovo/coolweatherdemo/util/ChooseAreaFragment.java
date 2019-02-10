079a84b9fa9c2c4135031241ac1fd5e38609202d
cathy -zrq <15911186361@163.com>
2019-02-10 18:46:21 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/lenovo/coolweatherdemo/util/ChooseAreaFragment.java
+++ app/src/main/java/com/example/lenovo/coolweatherdemo/util/ChooseAreaFragment.java
@@ -100,10 +100,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounies();
                 } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id", weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    } else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
@@ -225,7 +232,7 @@ public class ChooseAreaFragment extends Fragment {
                     result = Utility.handleCountyResponse(responseText, selectedCity.getId());
                 }
 
-                    if (result) {
+                if (result) {
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
